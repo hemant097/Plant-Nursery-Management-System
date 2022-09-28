76ca2472ff48637c4899bbcfebe9846f44936414
hemant097 <hem789ant1997@outlook.com>
2022-09-28 19:48:08 +0530

    .

---
+++ Plant_Nursery_Management/src/main/java/com/masai/models/demo.java
@@ -0,0 +1,13 @@
+package com.masai.models;
+
+public class demo {
+
+	public static void main(String[] args) {
+		
+		// i have created this class to check if the lombok
+		//is working or not
+		
+		Address a = new Address();
+		a.setCity("Kolkata");
+	}
+}
