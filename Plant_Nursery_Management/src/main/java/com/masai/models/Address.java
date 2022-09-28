97b1aea9616eca709b5ea3cba7a56368fc174359
Priyobrato Banik <reach2priyo@gmail.com>
2022-09-28 21:45:44 +0530

    updating id

--- Plant_Nursery_Management/src/main/java/com/masai/models/Address.java
+++ Plant_Nursery_Management/src/main/java/com/masai/models/Address.java
@@ -1,19 +1,31 @@
 package com.masai.models;
 
+
+
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 
 public class Address {
-Integer addressId;
-String houseNo;
-String colony;
-String city;
-String state;
-Integer pinCode;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+private Integer addressId;
+private String houseNo;
+private String colony;
+private String city;
+private String state;
+private Integer pinCode;
+
 
 }
--- Plant_Nursery_Management/src/main/java/com/masai/models/demo.java
+++ Plant_Nursery_Management/src/main/java/com/masai/models/demo.java
@@ -7,7 +7,7 @@ public class demo {
 		// i have created this class to check if the lombok
 		//is working or not
 		
-		Address a = new Address();
-		a.setCity("Kolkata");
+		Address a=new Address();
+		a.setCity("apd");
 	}
 }
