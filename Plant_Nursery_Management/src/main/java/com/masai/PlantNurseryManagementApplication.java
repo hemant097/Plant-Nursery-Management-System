e3f9befc1b1d850ac6fcb5d0e5836ff234eb507b
hemant097 <hem789ant1997@outlook.com>
2022-09-28 19:42:29 +0530

    .

--- Plant_Nursery_Management/src/main/java/com/masai/PlantNurseryManagementApplication.java
+++ Plant_Nursery_Management/src/main/java/com/masai/PlantNurseryManagementApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NurseryManagementSystemApplication {
+public class PlantNurseryManagementApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NurseryManagementSystemApplication.class, args);
+		SpringApplication.run(PlantNurseryManagementApplication.class, args);
 	}
 
 }
