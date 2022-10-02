83e91e82896eb92ee8d73878923779ee680205a1
Priyobrato Banik <reach2priyo@gmail.com>
2022-10-03 00:59:37 +0530

    cart initial added

--- Plant_Nursery_Management/src/main/java/com/masai/ServiceLayer/CartService.java
+++ Plant_Nursery_Management/src/main/java/com/masai/ServiceLayer/CartService.java
@@ -1,17 +1,20 @@
-//package com.masai.ServiceLayer;
-//import java.util.List;
-//import com.masai.models.Cart;
-//import com.masai.models.Customer;
-//
-//
-//public interface CartService {
-//	
-////	public Cart saveToCart( Customer customer ,Order order ) ;
-////	
-////	public List<Order> getAllItem(Cart cart);
-////	
-////	public Cart alterCart(Customer customer);
-//	
-//	
-//	
-//}
+package com.masai.ServiceLayer;
+import java.util.List;
+import com.masai.models.Cart;
+import com.masai.models.Customer;
+import com.masai.models.CustomerOrder;
+
+
+public interface CartService {
+	
+	public Cart addToCart( Integer CustomerId,Integer productId ) ;
+	
+	public Cart getAllItem(Cart cart);
+	
+	public String deleteFromCart(Integer Id);
+	
+	public String emptyCart();
+	
+	
+	
+}
